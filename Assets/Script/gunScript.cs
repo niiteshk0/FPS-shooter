33844c7ec3810f23c46fd1e99c30259008df41d6
Nitesh Kumar <nitesh.kgupta2002@gmail.com>
2024-01-23 14:40:35 +0530

    add muzzle flash and Gun reloading

--- Assets/Script/gunScript.cs
+++ Assets/Script/gunScript.cs
@@ -1,32 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class gunScript : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
-    public float impaceForce = 50f;
+    public float impactForce = 250f;
+    public float fireRate = 15f;
+    public ParticleSystem muzzleFlash;
 
+    float nextFireTime = 0f;
+
+    bool isReloading = false;
+    public int maxAmmo = 10;
+    int currentAmmo;
     public Camera fpsCam;
 
+    private void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if (isReloading)
+            return;
+
+        if(currentAmmo <= 0)
         {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if(Input.GetButton("Fire1") && Time.time >= nextFireTime)
+        {
+            nextFireTime = Time.time + 1f / fireRate;
             shoot();
         }
     }
 
+    IEnumerator Reload()
+    {
+        Debug.Log("Reloading");
+        isReloading = true;
+
+        yield return new WaitForSeconds(1f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
     void shoot()
     {
-        RaycastHit rayHit;
+        muzzleFlash.Play();
+        currentAmmo--;
 
+        RaycastHit rayHit;
+        
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out rayHit, range))
         {
             Debug.Log(rayHit.transform.name);   
 
-            if(rayHit.rigidbody != null)
+            if(rayHit.rigidbody != null)      // when goli touch with object then move the object
             {
-                rayHit.rigidbody.AddForce(-rayHit.normal * impaceForce);
+                rayHit.rigidbody.AddForce(-rayHit.normal * impactForce);
             }
 
             enemyScript Enemy = rayHit.transform.GetComponent<enemyScript>();
